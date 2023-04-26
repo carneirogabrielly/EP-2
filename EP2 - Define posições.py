059e205c0d34c4afe06e12fb2a96cbd434016e87
Lucas Santos <lucass4@al.insper.edu.br>
2023-04-26 19:48:09 -0300

    Função Posições

---
+++ EP2 - Define posições.py
@@ -0,0 +1,23 @@
+#  podemos pensar que ao acessar o elemento grid[0][1] estamos acessando a linha 0 e a coluna 1
+
+#  1. implementar uma função chamada define_posicoes
+
+# 2. recebe como argumentos uma linha, coluna, orientacao e um tamanho
+
+# 3. função retorna uma lista com as posições que o navio irá ocupar
+
+
+# linha = 2
+# coluna = 4
+# orientacao = "vertical"
+# tamanho = 3
+
+def define_posicoes (linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    else:
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    return posicoes
