2e1d15939c9ed8b81e436128eae7064801e19a1c
Gabrielly <gabriellycs@al.insper.edu.br>
2023-04-29 22:07:03 -0300

    atualização

--- EP2 - Preenche frota.py
+++ EP2 - Preenche frota.py
@@ -10,7 +10,7 @@
 
 def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     if nome_navio in frota:    
-        frota[nome_navio].extend([define_posicoes(linha, coluna, orientacao, tamanho)])
+        frota[nome_navio] += ([define_posicoes(linha, coluna, orientacao, tamanho)])
     else:
         frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
     return frota
