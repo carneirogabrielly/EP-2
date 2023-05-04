b762bfcbf0dc3f5bd1764e716e67162e31bdaa5f
Gabrielly <gabriellycs@al.insper.edu.br>
2023-05-04 14:53:51 -0300

    Posicionando Frota - quase certo

--- EP2 - Posicionando frota.py
+++ EP2 - Posicionando frota.py
@@ -85,10 +85,12 @@ embarcacao = {   #primeiro termo da lista é o tamanho, o segundo é a quantidad
 for nome_navio in embarcacao:      # vai dar o nome_navio do navio
     tamanho = embarcacao[nome_navio][0]
     qtd = embarcacao[nome_navio][1]
-
-    for i in range(qtd):  #vai repetir a pergunta das informações 
-        
-        print("Insira as informações referentes ao navio {} que possui tamanho {}".format(nome_navio,tamanho))
+    
+    i = 0
+    
+    while i <  qtd:
+    
+        print("Insira as informações referentes ao navio {0} que possui tamanho {1}".format(nome_navio,tamanho))
         linha = int(input("Qual a linha desejada para colocar a embarcação? "))
         coluna = int(input("Qual a coluna desejada para colocar a embarcação? "))
         if nome_navio != "submarino":
@@ -97,16 +99,21 @@ for nome_navio in embarcacao:      # vai dar o nome_navio do navio
                 orientacao = "vertical"
             elif orientacao == 2:
                 orientacao = "horizontal"
-        if nome_navio == "submarino":
+        else:
             orientacao = "horizontal" #escolha arbitrária
             
         #vai verificar se a posicao é valida
         
         if posicao_valida (frota, linha, coluna, orientacao, tamanho) == True:
             
+            #vai atualizar a frota
+            
             frota = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
             
+            i +=1
+        
+            
         elif posicao_valida (frota, linha, coluna, orientacao, tamanho) == False:
+            
             print ("Esta posição não está válida!")
             
-        
