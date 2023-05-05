62ec14dc4f5550715f336fdb55b9bfef4af3ba4c
Lucas Santos <lucass4@al.insper.edu.br>
2023-05-05 10:38:26 -0300

    SUCESSO !!

--- EP2 - Posicionando frota.py
+++ EP2 - Posicionando frota.py
@@ -68,12 +68,7 @@ def posicao_valida (frota, linha, coluna, orientacao, tamanho):
 
     return True
     
-frota = {
-    "porta-aviões":[],
-    "navio-tanque":[],
-    "contratorpedeiro":[],
-    "submarino": [],
-}
+frota = {}
 
 embarcacao = {   #primeiro termo da lista é o tamanho, o segundo é a quantidade de vezes que ele aparece
     "porta-aviões": [4,1],
@@ -85,17 +80,43 @@ embarcacao = {   #primeiro termo da lista é o tamanho, o segundo é a quantidad
 for nome_navio in embarcacao:      # vai dar o nome_navio do navio
     tamanho = embarcacao[nome_navio][0]
     qtd = embarcacao[nome_navio][1]
-
-    for i in range(qtd):  #vai repetir a pergunta das informações 
-        
-        print("Insira as informações referentes ao navio {} que possui tamanho {}".format(nome_navio,tamanho))
+    
+    i = 0
+    
+    while i < qtd:
+    
+        print("Insira as informações referentes ao navio {0} que possui tamanho {1}".format(nome_navio,tamanho))
         linha = int(input("Qual a linha desejada para colocar a embarcação? "))
         coluna = int(input("Qual a coluna desejada para colocar a embarcação? "))
         if nome_navio != "submarino":
-            orientacao = input("Qual a orientação desejada para colocar a embarcação? Digite 1 para vertical e 2 para horizontal")
+            orientacao = int(input("Qual a orientação desejada para colocar a embarcação? Digite 1 para vertical e 2 para horizontal "))
             if orientacao == 1:
                 orientacao = "vertical"
             elif orientacao == 2:
                 orientacao = "horizontal"
-        if nome_navio == "submarino":
+        else:
             orientacao = "horizontal" #escolha arbitrária
+            
+        #vai verificar se a posicao é valida
+        
+        if posicao_valida (frota, linha, coluna, orientacao, tamanho) == True:
+            
+            
+            #vai atualizar a frota
+            
+            frota = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
+            # preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
+            
+            i +=1
+            # if coluna == tamanho and i <= qtd:
+            #     break
+            #print(frota)
+            
+
+        elif posicao_valida (frota, linha, coluna, orientacao, tamanho) == False:
+            
+            print ("Esta posição não está válida!")
+            
+# print(frota)
+print(frota)
+
