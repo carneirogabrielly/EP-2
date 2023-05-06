41a4c42cd46aad822a5c7799a7f457015dd2b3f0
Gabrielly <gabriellycs@al.insper.edu.br>
2023-05-06 14:11:36 -0300

    Jogada do jogador - sem as outras funções

--- EP2 - Jogada do jogador.py
+++ EP2 - Jogada do jogador.py
@@ -1,116 +1,4 @@
-def define_posicoes(linha, coluna, orientacao, tamanho):
-    posicoes = []
-    if orientacao == "vertical":
-        for i in range(tamanho):
-            posicoes.append([linha + i, coluna])
-    else:
-        for i in range(tamanho):
-            posicoes.append([linha, coluna + i])
-    return posicoes
-
-def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
-    if nome_navio in frota:    
-        frota[nome_navio] += ([define_posicoes(linha, coluna, orientacao, tamanho)])
-    else:
-        frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
-    return frota
-
-def faz_jogada(tabuleiro, linha, coluna):
-    if tabuleiro[linha][coluna] == 0:
-        tabuleiro [linha][coluna] = '-'
-    elif tabuleiro[linha][coluna] == 1:
-        tabuleiro[linha][coluna] = 'X'
-    return tabuleiro
-
-def posiciona_frota(frota):
-    tabuleiro = []
-    for i in range(10):
-        tabuleiro.append([0]*10)
-    for navio in frota:
-        for posicao in frota[navio]:
-            for coordenada in posicao:
-                tabuleiro[coordenada[0]][coordenada[1]] = 1
-    return tabuleiro
-
-def afundados (frota, tabuleiro):
-    navios_afundados = 0
-    for navio in frota:
-        for posicao in frota[navio]:
-            for coordenada in posicao:
-                if tabuleiro[coordenada[0]][coordenada[1]] != 'X':
-                    break
-            else:
-                navios_afundados += 1
-    return navios_afundados
-
-def posicao_valida (frota, linha, coluna, orientacao, tamanho):
-
-    novas_posicoes = define_posicoes (linha, coluna, orientacao, tamanho)       #novas_posições armazena as posições definidas pelo jogador, é uma lista de listas com dois valores
-
-    #agora eu quero percorrer todas as coordenadas dessa lista e ver se tem alguma igual
-
-    for navio in frota:
-        for posicao in frota[navio]:
-            for coordenada in posicao:
-                for i in range (len(novas_posicoes)):
-                    if novas_posicoes[i] == coordenada:
-                        return False
-
-    for i in range(len(novas_posicoes)):
-
-        if novas_posicoes[i][0] >=10:
-            return False
-        elif novas_posicoes[i][1] >=10:
-            return False
-
-    return True
-    
-frota = {}
-
-embarcacao = {   #primeiro termo da lista é o tamanho, o segundo é a quantidade de vezes que ele aparece
-    "porta-aviões": [4,1],
-    "navio-tanque": [3,2],
-    "contratorpedeiro": [2,3],
-    "submarino": [1,4],
-}
-
-for nome_navio in embarcacao:      # vai dar o nome_navio do navio
-    tamanho = embarcacao[nome_navio][0]
-    qtd = embarcacao[nome_navio][1]
-    
-    i = 0
-    
-    while i < qtd:
-    
-        print("Insira as informações referentes ao navio {0} que possui tamanho {1}".format(nome_navio,tamanho))
-        linha = int(input("Qual a linha desejada para colocar a embarcação? "))
-        coluna = int(input("Qual a coluna desejada para colocar a embarcação? "))
-        if nome_navio != "submarino":
-            orientacao = int(input("Qual a orientação desejada para colocar a embarcação? Digite 1 para vertical e 2 para horizontal "))
-            if orientacao == 1:
-                orientacao = "vertical"
-            elif orientacao == 2:
-                orientacao = "horizontal"
-        else:
-            orientacao = "horizontal" #escolha arbitrária
-            
-        #vai verificar se a posicao é valida
         
-        if posicao_valida (frota, linha, coluna, orientacao, tamanho) == True:
-            
-            
-            #vai atualizar a frota
-            
-            frota = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
-            
-            i +=1
-            
-
-        elif posicao_valida (frota, linha, coluna, orientacao, tamanho) == False:
-            
-            print ("Esta posição não está válida!")
-            
-          
 frota_oponente = {
     'porta-aviões': [
         [[9, 1], [9, 2], [9, 3], [9, 4]]
@@ -201,7 +89,7 @@ while jogando:
         
         #primeiro vou ter que contar quantos navios o oponente tinha
         #ja fiz isso antes do inicio do loop, ta armazenado em quantidade_navios
-        
+        #se a quantidade de navios afundados foi igual a quantidade de navios, acaba o jogo
         
         navios_afundados = afundados(frota_oponente, tabuleiro_oponente)
         if navios_afundados == quantidade_navios:
